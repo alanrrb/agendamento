f9248f41b6c8a681f8aef3f58a616c6ecaa90325
Daniel <daniel.passarini@gmail.com>
2015-09-07 15:58:46 -0300

    adicionando acesso a nome e sobrenome ao devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
 
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :nome_completo
+    devise_parameter_sanitizer.for(:sign_up) << :nome
+    devise_parameter_sanitizer.for(:sign_up) << :sobrenome
   end
 end
